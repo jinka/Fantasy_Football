dbb06cafe2896b2e0072f7975de9acfac3755057
derrrick william <williamderrick100@gmail.com>
2019-02-20 14:24:29 +0300

    Routing for the login and profile pages.

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,46 @@ from .. import db
 
 @main.route('/')
 def index():
-    return '<h1> Welcome to Football Fantasy </h1>'
+    '''
+    View root page function that returns the index page and its data
+
+    '''
+    return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
