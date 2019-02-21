d0c3f6dc8d17fdd2dc046a45e01855c2b176e179
derrrick william <williamderrick100@gmail.com>
2019-02-21 16:46:24 +0300

    Finished Forms.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, PasswordField,BooleanField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField,BooleanField, RadioField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
@@ -14,9 +14,12 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
+    interest = RadioField('Select Choice', choices=[('fixture','Fixtures'),('live match','Live Games')])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+
 
     def validate_email(self,data_field):
             if User.query.filter_by(email = data_field.data).first():
