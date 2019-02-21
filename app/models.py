4b26aa534f74b1ed74205ae1a8151fb91ea37b6c
derrrick william <williamderrick100@gmail.com>
2019-02-21 05:01:41 +0300

    changes on user class models.

--- app/models.py
+++ app/models.py
@@ -9,6 +9,28 @@ class User(db.Model):
 
     id = db.Column(db.Integer,primary_key =True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index =True)
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String)
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 
     def __repr__(self):
         return f'User {self.username}'
